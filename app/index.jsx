1006e21935b8988b294b603da8cab394bcabce9c
char-lie <5962274+char-lie@users.noreply.github.com>
2017-02-10 15:42:26 +0200

    Displaying button in application

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 
+import HelloButton from './HelloButton'
 import HelloMessage from './HelloMessage'
 
-render(<div><HelloMessage/></div>, document.getElementById('app'));
+render(<div><HelloMessage/><HelloButton/></div>, document.getElementById('app'));
