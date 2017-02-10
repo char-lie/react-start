c06b45cb2d5952ff92e02c7b2640fc2ba02beff2
Valerii Kryhin <valerii.kryhin@avid.com>
2017-02-10 15:15:39 +0200

    Added WebPack configuration file

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    context: path.join(__dirname, 'app'),
+    entry: './index',
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    }
+};
