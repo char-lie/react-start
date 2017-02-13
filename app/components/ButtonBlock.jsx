ce5e78cdc80a5d0b4105e781be6888d538e8f5fb
char-lie <5962274+char-lie@users.noreply.github.com>
2017-02-13 15:18:50 +0200

    Added new button to get text from input

--- app/components/ButtonBlock.jsx
+++ app/components/ButtonBlock.jsx
@@ -1,9 +1,10 @@
 import React, {PropTypes} from 'react';
 
-const ButtonBlock = ({onClick}) => (
+const ButtonBlock = ({onClick, onExternalClick}) => (
     <div>
         <button onClick={() => onClick('Text1')}>Set text1</button>
         <button onClick={() => onClick('Text2')}>Set text2</button>
+        <button onClick={() => onExternalClick()}>Set text from input</button>
     </div>
 )
 
