7b373a0748f8c2b6585ec7bd513924ddfcb8bebf
char-lie <5962274+char-lie@users.noreply.github.com>
2017-02-13 14:23:55 +0200

    Converted simple DOM to separate stateful component
    Connected to store and dispatching text change

--- app/components/index.jsx
+++ app/components/index.jsx
@@ -1,6 +1,28 @@
-import React from 'react';
+import React from 'react'
+import {connect} from 'react-redux'
 
 import ButtonBlock from './ButtonBlock'
 import HelloMessage from './HelloMessage'
 
-export default (<div><HelloMessage/><ButtonBlock/></div>)
+import {makeText} from '../actions/makeHello'
+
+class Application extends React.Component {
+    constructor(props) {
+        super(props)
+        this.onButtonClick = this.onButtonClick.bind(this)
+    }
+
+    render() {
+        return (
+            <div>
+                <HelloMessage text={this.props.text}/>
+                <ButtonBlock onClick={this.onButtonClick}/>
+            </div>
+        )
+    }
+    onButtonClick(text) {
+        this.props.dispatch(makeText(text))
+    }
+}
+
+export default connect((state) => ({text: state.changeText.text}))(Application)
