d836218dc8f7589df746e260ae6678640e94c115
char-lie <5962274+char-lie@users.noreply.github.com>
2017-02-13 15:19:04 +0200

    Linked text input, button and text

--- app/components/index.jsx
+++ app/components/index.jsx
@@ -3,6 +3,7 @@ import {connect} from 'react-redux'
 
 import ButtonBlock from './ButtonBlock'
 import HelloMessage from './HelloMessage'
+import TextInput from './TextInput'
 
 import {makeText} from '../actions/makeHello'
 
@@ -10,18 +11,23 @@ class Application extends React.Component {
     constructor(props) {
         super(props)
         this.onButtonClick = this.onButtonClick.bind(this)
+        this.onAdvancedButtonClick = this.onAdvancedButtonClick.bind(this)
     }
 
     render() {
         return (
             <div>
                 <HelloMessage text={this.props.text}/>
-                <ButtonBlock onClick={this.onButtonClick}/>
+                <ButtonBlock onClick={this.onButtonClick} onExternalClick={this.onAdvancedButtonClick}/>
+                <TextInput ref="myTextInput"/>
             </div>
         )
     }
     onButtonClick(text) {
         this.props.dispatch(makeText(text))
+    }
+    onAdvancedButtonClick() {
+        this.props.dispatch(makeText(this.refs.myTextInput.getInputValue()))
     }
 }
 
