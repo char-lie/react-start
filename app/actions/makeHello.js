4704cb653c0a6c6eebc2e5c06395c39363f02d0c
char-lie <5962274+char-lie@users.noreply.github.com>
2017-02-13 14:22:01 +0200

    Implemented text changing actions

---
+++ app/actions/makeHello.js
@@ -0,0 +1,12 @@
+const MAKE_TEXT = 'MAKE_TEXT'
+
+export const Actions = {
+    MAKE_TEXT
+}
+
+export function makeText(text) {
+    return {
+        type: MAKE_TEXT,
+        payload: text
+    }
+}
