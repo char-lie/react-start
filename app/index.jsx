50bc33f58e3d4d8897d02ba20a09c3a0617c2d24
char-lie <5962274+char-lie@users.noreply.github.com>
2017-02-13 14:23:24 +0200

    Providing store and reducers to DOM structure

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,15 @@
-import {render} from 'react-dom';
+import React from 'react'
+import {render} from 'react-dom'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
 
 import DOMStructure from './components/index'
+import reducers from './reducers'
 
-render(DOMStructure, document.getElementById('app'));
+const store = createStore(reducers)
+
+render((
+    <Provider store={store}>
+        <DOMStructure/>
+    </Provider>
+), document.getElementById('app'))
