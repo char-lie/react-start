ccee6ec9e90b18efee53b61b84bc7ada715121b4
char-lie <5962274+char-lie@users.noreply.github.com>
2017-02-13 14:24:41 +0200

    Applying store text to text on the page

--- app/components/HelloMessage.jsx
+++ app/components/HelloMessage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 class HelloMessage extends React.Component {
     render() {
-        return <div>Hello world 2 + 2 = {2+2}</div>;
+        return <div>{this.props.text}</div>;
     }
 }
 
