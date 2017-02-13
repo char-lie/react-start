649808496777991cd7ce46a9b7ee01d088fdf858
char-lie <5962274+char-lie@users.noreply.github.com>
2017-02-13 16:36:37 +0200

    Added custom style to a button

--- app/components/ButtonBlock.jsx
+++ app/components/ButtonBlock.jsx
@@ -2,7 +2,7 @@ import React, { PropTypes } from 'react';
 
 const ButtonBlock = ({ onClick, onExternalClick }) => (
   <div>
-    <button onClick={() => onClick('Text1')}>Set text1</button>
+    <button className="custom-button" onClick={() => onClick('Text1')}>Set text1</button>
     <button onClick={() => onClick('Text2')}>Set text2</button>
     <button onClick={() => onExternalClick()}>Set text from input</button>
   </div>
