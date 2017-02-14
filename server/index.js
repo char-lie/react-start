de13e8cfdaf314df8543ad658add243241251098
char-lie <5962274+char-lie@users.noreply.github.com>
2017-02-14 15:51:31 +0200

    Allowed cross-site scripting in ExpressJS server

--- server/index.js
+++ server/index.js
@@ -11,12 +11,20 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'index.html'));
 });
 
+app.options('/get-random', (req, res) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.header('Access-Control-Allow-Methods', 'GET');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.send(200);
+});
+
 app.get('/get-random', (req, res) => {
     const result = [];
     for (let i = 0; i < 10; i++) {
         result.push(Math.random());
     }
-    res.json(result);
+    res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+    res.json({data: result});
 });
 
 app.listen(3000, () => {
