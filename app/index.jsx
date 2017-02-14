91277100260ecd817e4fcc847b9989c4e4ae0582
char-lie <5962274+char-lie@users.noreply.github.com>
2017-02-14 17:11:44 +0200

    Bound Promise middleware to Redux store

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import promiseMiddleware from 'redux-promise-middleware';
 
 import './sass/index.scss';
 
 import DOMStructure from './components/index';
 import reducers from './reducers';
 
-const store = createStore(reducers);
+const composeStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware(),
+)(createStore);
+
+const store = composeStoreWithMiddleware(reducers);
 
 render((
   <Provider store={store}>
