dbd799913271d50f9bd065b403bccb77e6dfac58
char-lie <5962274+char-lie@users.noreply.github.com>
2017-02-14 17:02:48 +0200

    Added button to change table content

--- app/components/ButtonBlock.jsx
+++ app/components/ButtonBlock.jsx
@@ -1,16 +1,18 @@
 import React, { PropTypes } from 'react';
 
-const ButtonBlock = ({ onClick, onExternalClick }) => (
+const ButtonBlock = ({ onClick, onExternalClick, onChangeTable }) => (
   <div>
     <button className="custom-button" onClick={() => onClick('Text1')}>Set text1</button>
     <button onClick={() => onClick('Text2')}>Set text2</button>
     <button onClick={() => onExternalClick()}>Set text from input</button>
+    <button onClick={() => onChangeTable()}>Get random numbers</button>
   </div>
 );
 
 ButtonBlock.propTypes = {
   onClick: PropTypes.func.isRequired,
   onExternalClick: PropTypes.func.isRequired,
+  onChangeTable: PropTypes.func.isRequired,
 };
 
 export default ButtonBlock;
